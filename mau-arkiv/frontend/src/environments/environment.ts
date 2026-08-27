7ed648d579a7a086fc4b448cbd05f303410de9ff
LasseCongo <paul.bergstrom@mau.se>
2026-08-27 14:18:47 +0200

    Update environment.ts

--- mau-arkiv/frontend/src/environments/environment.ts
+++ mau-arkiv/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: '2.0.2', // This should match frontend/package.json version
+  version: '2.0.1', // This should match frontend/package.json version
   api: {
     url: 'http://localhost:3000/api'
   },
